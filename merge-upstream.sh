7870b856d852a6b4293dd9bcbae0f789d9b3538f
zdm <zdm@softvisio.net>
2025-05-30 18:42:53 +0300

    chore: update merge script

--- merge-upstream.sh
+++ merge-upstream.sh
@@ -5,18 +5,18 @@ set -e
 git fetch upstream
 
 git switch update-readme
-git merge upstream/main
+git merge --no-edit upstream/main
 
 git switch feat-msys2
-git merge upstream/main
+git merge --no-edit upstream/main
 
 git switch feat-git-crypt
-git merge upstream/main
+git merge --no-edit upstream/main
 
 git switch feat-git-option
-git merge upstream/main
+git merge --no-edit upstream/main
 
 git switch main
-git merge update-readme feat-msys2 feat-git-crypt feat-git-option
+git merge --no-edit update-readme feat-msys2 feat-git-crypt feat-git-option
 
 git push --all
