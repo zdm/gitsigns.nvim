65442271ea9d053d021aab50d2e50f8d44b74256
zdm <zdm@softvisio.net>
2025-06-24 14:42:23 +0300

    refactor: update bash scripts errors handling

--- merge-upstream.sh
+++ merge-upstream.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -Eeuo pipefail
-trap 'echo -e "\n⚠  Warning: A command has failed. Line ($0:$LINENO): $(sed -n "${LINENO}p" "$0" 2> /dev/null || true)" >&2; return 3 2> /dev/null || exit 3' ERR
+trap 'echo -e "⚠  Warning: A command has failed. Line ($0:$LINENO): $(sed -n "${LINENO}p" "$0" 2> /dev/null || true)" >&2; return 3 2> /dev/null || exit 3' ERR
 
 git fetch upstream
 
