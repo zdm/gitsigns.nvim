411c56733ce951b4f0a8b03b8484da60da04d3b6
zdm <zdm@softvisio.net>
2025-09-26 08:02:29 +0300

    fix: fix merge script

--- merge-upstream.sh
+++ merge-upstream.sh
@@ -3,7 +3,17 @@
 set -Eeuo pipefail
 trap 'echo -e "⚠  Error ($0:$LINENO): $(sed -n "${LINENO}p" "$0" 2> /dev/null | grep -oE "\S.*\S|\S" || true)" >&2; return 3 2> /dev/null || exit 3' ERR
 
+old_commit=$(git rev-parse upstream)
+
 git fetch upstream
+
+new_commit=$(git rev-parse upstream)
+
+if [ "$old_commit" = "$new_commit" ]; then
+  echo "No updates available"
+
+  exit
+fi
 
 git switch update-readme
 git merge --no-edit upstream/main
